d995137f0eed6dd644aa7a6192e3aec704f138f5
Matt Ray <matthewhray@gmail.com>
2014-07-17 22:41:23 -0500

    refactored to use knife-cloud-style lists

--- lib/chef/knife/vchs_vm_list.rb
+++ lib/chef/knife/vchs_vm_list.rb
@@ -1,13 +1,25 @@
 #
-# Author:: Seth Thomas
-# Copyright:: 
+# Author:: Matt Ray (<matt@getchef.com>)
+# Author:: Seth Thomas (<sthomas@getchef.com>)
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 require 'chef/knife/cloud/list_resource_command'
 require 'chef/knife/vchs_helpers'
 require 'chef/knife/cloud/vchs_service_options'
-
-# TODO - Your cloud may or may not support this. If no, simply delete this file.
 
 class Chef
   class Knife
@@ -19,44 +31,32 @@ class Chef
         banner "knife vchs vm list (options)"
 
         def query_resource
-          begin
-          @service.connection.organizations
-
-          rescue Excon::Errors::BadRequest => e
-            response = Chef::JSONCompat.from_json(e.response.body)
-            ui.fatal("Unknown server error (#{response['badRequest']['code']}): #{response['badRequest']['message']}")
-            raise e
+          vdc = @service.connection.organizations.get_by_name(Chef::Config[:knife][:vchs_org]).vdcs.first
+          vms = []
+          vdc.vapps.all.each do |vapp|
+            vms << vapp.vms.all
           end
+          vms.flatten
         end
 
-           
-      	def list(organizations)
-          vm_list = [
-            ui.color('Name', :bold),
-            ui.color('OS', :bold),
-            ui.color('IP', :bold),
-            ui.color('vAPP', :bold),
-            ui.color('CPU', :bold),
-            ui.color('Status', :bold),
+        def before_exec_command
+          @columns_with_info = [
+            {:label => 'vAPP', :key => 'vapp_name'},
+            {:label => 'Name', :key => 'name'},
+            {:label => 'IP', :key => 'ip_address'},
+            {:label => 'CPU', :key => 'cpu'},
+            {:label => 'Memory', :key => 'memory'},
+            {:label => 'OS', :key => 'operating_system'},
+            {:label => 'Owner', :key => 'vapp', :value_callback => method(:owner) },
+            {:label => 'Status', :key => 'status'}
           ]
-          org = organizations.get_by_name(Chef::Config[:knife][:vchs_org])
-          vdcs = org.vdcs.all
-          if vdcs
-            for vdc in vdcs
-              vdc.vapps.all.each do |vapp|
-                vapp.vms.sort_by(&:name).each do |vm|
-                  vm_list << vm.name
-                  vm_list << vm.operating_system
-                  vm_list << vm.ip_address
-                  vm_list << vm.vapp_name
-                  vm_list << vm.cpu.to_s
-                  vm_list << vm.status
-                end
-              end
-            end
-          end
-        puts ui.list(vm_list, :uneven_columns_across, 6)
+          @sort_by_field = 'vapp_name'
         end
+
+        def owner(vapp)
+          vapp.owner[0][:name]
+        end
+
       end
     end
   end
