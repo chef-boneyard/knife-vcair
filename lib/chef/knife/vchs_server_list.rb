85371d7582e90029f874dccea0be8905548372b6
Seth Thomas <sthomas@getchef.com>
2014-07-18 09:03:34 -0700

    server list options now match vm list

--- lib/chef/knife/vchs_server_list.rb
+++ lib/chef/knife/vchs_server_list.rb
@@ -4,11 +4,13 @@
 #
 
 require 'chef/knife/vchs_vm_list'
+require 'chef/knife/cloud/vchs_service_options'
 
 class Chef
   class Knife
     class Cloud
       class VchsServerList < VchsVmList
+        include VchsServiceOptions
 
         banner "knife vchs server list (options)"
 
