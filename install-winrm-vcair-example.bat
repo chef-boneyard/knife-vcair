64150a7b172dab1374bf4ca6674f4c2f33cd9630
Seth Thomas <sthomas@getchef.com>
2014-08-25 16:30:28 -0700

    Standardizing on advfirewall syntax (netsh is deprecated)

--- install-winrm-vcair-example.bat
+++ install-winrm-vcair-example.bat
@@ -21,9 +21,9 @@ echo %DATE% %TIME% > C:\vm-is-customized
 @rem Second Boot / start winrm, just incase, and fix firewall
 cmd.exe /c net start winrm 
 cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985 for WinRM"
-cmd.exe /c netsh advfirewall firewall set rule name="RDP" profile=public protocol=tcp localport=3389 remoteip=localsubnet new remoteip=any
+cmd.exe /c netsh advfirewall firewall set rule name="WinRM" profile=public protocol=tcp localport=5985 remoteip=localsubnet new remoteip=any
 cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+cmd.exe /c netsh advfirewall firewall set rule name="RDP" profile=public protocol=tcp localport=3389 remoteip=localsubnet new remoteip=any
 
 @rem Password Setting, Force Password Change, and Autologin currently seem broken
 @rem Forcing the password here at least allows us to connect remotely
